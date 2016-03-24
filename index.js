090319fccca128ef3e04a022660ea15be0262b50
tomkp <tom@tomkp.com>
2016-03-24 21:50:44 +0000

    ...

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 'use strict';
 const meow = require('meow');
-const logSymbols = require('log-symbols');
 const chalk = require('chalk');
 const tlv = require('tlv');
 const hexify = require('hexify');
 
+
+
+const cli = meow(``, {
+    string: ['_']
+});
+
+const input = cli.input[0];
+
+if (!input) {
+    console.error('TLV required');
+    process.exit(1);
+}
+
+const bytes = hexify.toByteArray(input);
+const parsedTlv = tlv.parse(new Buffer(bytes));
 
 function leftpad (str, len, ch) {
     str = String(str);
@@ -16,55 +30,37 @@ function leftpad (str, len, ch) {
     }
     return str;
 }
-
-
-const cli = meow(`
-	Usage
-	  $ npm-name <name>
-	Examples
-	  $ npm-name chalk
-	  ${logSymbols.error} ${chalk.bold('chalk')} is unavailable
-	  $ npm-name unicorn-cake
-	  ${logSymbols.success} ${chalk.bold('unicorn-cake')} is available
-	Exits with code 0 when the name is available or 2 when taken
-`, {
-    string: ['_']
-});
-
-const input = cli.input[0];
-
-if (!input) {
-    console.error('TLV required');
-    process.exit(1);
+function formatTag(tag) {
+    return chalk.red(tag.toString('16'));
 }
-
-const bytes = hexify.toByteArray(input)
-var parsedTlv = tlv.parse(new Buffer(bytes));
-
+function formatLength(length) {
+    return chalk.blue(leftpad(length, 2, '0'));
+}
 function formatValue(value) {
-    const ascii = chalk.green(value.toString());
-    const hex = chalk.cyan(value.toString('hex'));
+    const ascii = chalk.green.bgWhite(value.toString());
+    const hex = chalk.magenta.bgWhite(value.toString('hex'));
     return `${hex} ${ascii}`;
 }
 
 function format(tlv, index) {
     index++;
-    const tabs = Array(index).join('   ');
+    const tabs = new Array(index).join('   ');
+
+
+
     if (tlv.constructed) {
-        const tagOut = chalk.red(tlv.tag.toString('16'))
-        const lengthOut = chalk.blue(tlv.originalLength)
-        console.log(`${tabs} ${tagOut} ${lengthOut}`)
+        const tagOut = formatTag(tlv.tag);
+        const lengthOut = formatLength(tlv.originalLength);
         const arr = tlv.value;
         arr.map(function(child) {
             format(child, index);
         });
     } else {
-        const tagOut = chalk.red(tlv.tag.toString('16'))
-        const lengthOut = chalk.blue(tlv.originalLength)
+        const tagOut = formatTag(tlv.tag);
+        const lengthOut = formatLength(tlv.originalLength);
         var valueOut = formatValue(tlv.value);
         console.log(`${tabs} ${tagOut} ${lengthOut} ${valueOut}`)
     }
 }
 
 format(parsedTlv, 0);
-//console.log(chalk.blue(parsedTlv));
